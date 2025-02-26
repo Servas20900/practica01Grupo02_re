870843740f79d306224ac2fb9446a01b5c938f7b
Sebastián Roberto Méndez Artavia <sebasmendeza09@gmail.com>
2025-02-25 18:15:15 -0600

    backend
    servas

---
+++ práctica01Grupo02/src/main/java/practica01Grupo02/controller/ArbolController.java
@@ -0,0 +1,43 @@
+package practica01Grupo02.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import practica01Grupo02.domain.Arbol;
+import practica01Grupo02.service.ArbolService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/arboles")
+public class ArbolController {
+
+    @Autowired
+    private ArbolService arbolService;
+
+    @GetMapping("/")
+    public String listarArboles(Model model) {
+        List<Arbol> arboles = arbolService.getArboles(true);
+        model.addAttribute("arboles", arboles);
+        return "listarArboles"; // Redirige a listarArboles.html
+    }
+
+    @GetMapping("/nuevo")
+    public String nuevoArbol(Model model) {
+        model.addAttribute("arbol", new Arbol());
+        return "formArbol";
+    }
+
+    @PostMapping("/guardar")
+    public String guardarArbol(@ModelAttribute Arbol arbol) {
+        arbolService.save(arbol);
+        return "redirect:/arboles/";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminarArbol(@PathVariable Long id) {
+        arbolService.delete(id);
+        return "redirect:/arboles/";
+    }
+}
