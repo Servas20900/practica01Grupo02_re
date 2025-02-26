7302228bf099df733892687dfacca6fcb1f4c831
Sebastián Roberto Méndez Artavia <sebasmendeza09@gmail.com>
2025-02-26 14:17:42 -0600

    opa

--- práctica01Grupo02/src/main/java/practica01Grupo02/controller/ArbolController.java
+++ práctica01Grupo02/src/main/java/practica01Grupo02/controller/ArbolController.java
@@ -4,12 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import lombok.extern.slf4j.Slf4j;
 import practica01Grupo02.domain.Arbol;
 import practica01Grupo02.service.ArbolService;
+
 
 import java.util.List;
 
 @Controller
+@Slf4j
 @RequestMapping("/Arbol")
 public class ArbolController {
 
