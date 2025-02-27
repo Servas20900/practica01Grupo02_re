2897d178de7195860ebfede6cd5e6462dc54678d
Sebastián Roberto Méndez Artavia <sebasmendeza09@gmail.com>
2025-02-27 13:58:26 -0600

    update

---
+++ practicaExGrupo02/src/test/java/practicaExGrupo02/practicaEx/PracticaExApplicationTests.java
@@ -0,0 +1,13 @@
+package practicaExGrupo02.practicaEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
