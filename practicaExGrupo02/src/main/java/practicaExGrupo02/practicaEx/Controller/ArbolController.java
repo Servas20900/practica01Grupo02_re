82f6e9fb9484ea9569595972047dc369a04539cd
jzamora621 <jzamora60021@ufide.ac.cr>
2025-02-28 21:11:40 -0600

    Controller y Service Imp

---
+++ practicaExGrupo02/src/main/java/practicaExGrupo02/practicaEx/Controller/ArbolController.java
@@ -0,0 +1,27 @@
+package practicaExGrupo02.practicaEx.Controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import practicaExGrupo02.practicaEx.service.ArbolService;
+
+
+@Controller
+@RequestMapping("/arbol")
+public class ArbolController {
+    
+    @Autowired
+    private ArbolService arbolService ;
+
+    @GetMapping("/listado")
+    public String listarMedicamentos(Model model) {
+        var arboles = arbolService.getArboles();
+        model.addAttribute("arboles", arboles);
+        model.addAttribute("totalarboles", arboles.size());
+        return "/arbol/listado";
+    }
+}
--- practicaExGrupo02/src/main/java/practicaExGrupo02/practicaEx/service/impl/ArbolServiceImpl.java
+++ practicaExGrupo02/src/main/java/practicaExGrupo02/practicaEx/service/impl/ArbolServiceImpl.java
@@ -1,2 +1,23 @@
 package practicaExGrupo02.practicaEx.service.impl;
 
+import practicaExGrupo02.practicaEx.dao.ArbolDao;
+import practicaExGrupo02.practicaEx.domain.Arbol;
+import practicaExGrupo02.practicaEx.service.ArbolService;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ArbolServiceImpl implements ArbolService {
+    
+    @Autowired
+    private ArbolDao arbolDao;
+
+    @Override
+    @Transactional(readOnly=true)
+    public List<Arbol> getArboles() {
+        return arbolDao.findAll();
+    }
+}
