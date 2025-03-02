15bc4c772b078279e0b00cea4ab25cf08422f34b
Sebastián Roberto Méndez Artavia <sebasmendeza09@gmail.com>
2025-03-01 23:34:45 -0600

    update
    update

--- practicaExGrupo02/src/main/java/practicaExGrupo02/practicaEx/dao/ArbolDao.java
+++ practicaExGrupo02/src/main/java/practicaExGrupo02/practicaEx/dao/ArbolDao.java
@@ -1,8 +1,10 @@
 package practicaExGrupo02.practicaEx.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import practicaExGrupo02.practicaEx.domain.Arbol;
 
+@Repository
 public interface ArbolDao extends JpaRepository <Arbol,Long>{
 
 }
--- practicaExGrupo02/src/main/java/practicaExGrupo02/practicaEx/service/ArbolService.java
+++ practicaExGrupo02/src/main/java/practicaExGrupo02/practicaEx/service/ArbolService.java
@@ -2,8 +2,9 @@ package practicaExGrupo02.practicaEx.service;
 
 import practicaExGrupo02.practicaEx.domain.Arbol;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public interface ArbolService {
     List<Arbol> getArboles();
 
